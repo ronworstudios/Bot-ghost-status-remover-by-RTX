f0d7bee6e0895c520412607287b0340a9e5f8b67
dadc0rd <166328637+dadc0rd@users.noreply.github.com>
2024-04-12 16:16:16 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Verifying members","Watching Yanna server"];
 
 
 let currentIndex = 0;
